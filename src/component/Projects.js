ef8234b600b112399db9b191d0566610fd41d6f9
pratik_03 <pratikagnihotri1234@gmail.com>
2022-10-09 15:38:23 +0530

    some changes

---
+++ src/component/Projects.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <div className="projects">
+        <div className="title">
+          <p>
+            Here Are Some Of My<br></br>Projects
+          </p>
+        </div>
+        <div className="topic">
+          <div className="top1">
+            <div className="para">
+              <h2>Fitho</h2>
+              <p>
+                Fitho is webapp project which works with the health sector. It
+                is basically a fitness app.
+              </p>
+              <div className="github">
+                <a href="https://github.com/King0311/FITHO">GitHub Repo</a>
+              </div>
+            </div>
+          </div>
+          <div className="top2">
+            <div className="para">
+              <h2>News-World</h2>
+              <p>
+                News-World fetch real time latest news from the api and display it.
+              </p>
+              <div className="github">
+                <a href="#">GitHub Repo</a>
+              </div>
+            </div>
+          </div>
+          <div className="top2">
+            <div className="para">
+              <h2>News-World</h2>
+              <p>
+                News-World fetch real time latest news from the api and display it.
+              </p>
+              <div className="github">
+                <a href="#">GitHub Repo</a>
+              </div>
+            </div>
+          </div>
+          <div className="top2">
+            <div className="para">
+              <h2>News-World</h2>
+              <p>
+                News-World fetch real time latest news from the api and display it.
+              </p>
+              <div className="github">
+                <a href="#">GitHub Repo</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
